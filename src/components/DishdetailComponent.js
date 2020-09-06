e1e53b9076d8be8221f5a1fe299ec4e6ddc3d20f
rayenbensaad <rayanbensaad@hotmail.fr>
2020-09-06 12:46:03 +0100

    thunk + middlware actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem,
-    Button, Modal, ModalHeader, ModalBody, Col, Row, Label, FormGroup } from 'reactstrap';
+    Button, Modal, ModalHeader, ModalBody, Label, FormGroup } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -59,7 +60,25 @@ function RenderComments({comments, addComment, dishId}) {
 }
 
 const DishDetail = (props) => {
-    if (props.dish != null) {
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null) {
         return(
             <div className="container">
                 <div className="row">
--- src/redux/dishes.js
+++ src/redux/dishes.js
@@ -1,8 +1,21 @@
-import { DISHES } from '../shared/dishes';
+import * as ActionTypes from './ActionTypes';
 
-export const Dishes = (state = DISHES, action) => {
-    switch (action.type) {
+export const Dishes = (state = {
+        isLoading: true,
+        errMess: null,
+        dishes: []
+    }, action) => {
+    switch(action.type) {
+        case ActionTypes.ADD_DISHES:
+            return {...state, isLoading: false, errMessL: null, dishes: action.payload};     //return immutable 
+
+        case ActionTypes.DISHES_LOADING:
+            return {...state, isLoading: true, errMessL: null, dishes: []}; 
+
+        case ActionTypes.DISHES_FAILED:
+            return {...state, isLoading: false, errMessL: action.payload, dishes: []};
+
         default:
-          return state;
-      }
-};
+            return state;
+    }
+}
