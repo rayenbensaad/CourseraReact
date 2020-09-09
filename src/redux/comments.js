3a6a5fa511cc77f4be3cc1ec19ff98f84e8dd521
rayenbensaad <rayanbensaad@hotmail.fr>
2020-09-09 22:57:04 +0100

    Fetch from Server

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -1,16 +1,23 @@
-import { COMMENTS } from '../shared/comments';
 import * as ActionTypes from './ActionTypes';
 
-export const Comments = (state = COMMENTS, action) => {
-    switch (action.type) {
+export const Comments = (state = {
+        errMess: null,
+        comments: []
+}, action) => {
+    switch(action.type) {
+        case ActionTypes.ADD_COMMENTS:
+            return {...state, isLoading: false, errMessL: null, comments: action.payload};
+
+        case ActionTypes.COMMENTS_FAILED:
+            return {...state, isLoading: false, errMessL: action.payload, comments: []};
+
         case ActionTypes.ADD_COMMENT:
             var comment = action.payload;
-            comment.id = state.length;
+            comment.id = state.comments.length;
             comment.date = new Date().toISOString();
-            console.log("Comment: ", comment);
-            return state.concat(comment);
+            return {...state, comments: state.comments.concat(comment)};       // create a new state
 
         default:
-          return state;
-      }
-};
+            return state;
+    }
+}
